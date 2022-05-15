d52bcf414c9c313fc39ed6801a937c745de6184b
Rafael Tavares <rafatavares03@hotmail.com>
2022-05-15 18:42:53 -0300

    scss variables set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-async function getAdvice() {
-  const url = `https://api.adviceslip.com/advice`
+async function GetAdvice() {
+  const url = `https://api.adviceslip.com/advice`;
   const results = await fetch(url);
   const json = await results.json();
   console.log(json);
-
 }
 
-getAdvice();
+GetAdvice();
