ad69232c259276396717b6538257a560aa94ace5
Rafael Tavares <rafatavares03@hotmail.com>
2022-05-15 22:41:19 -0300

    changing the advice

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,29 @@ async function GetAdvice() {
   const url = `https://api.adviceslip.com/advice`;
   const results = await fetch(url);
   const json = await results.json();
-  console.log(json);
+  if (results.status === 200) {
+    WriteAdvice({
+      advice: `"${json.slip.advice}"`,
+      number: `Advice #${json.slip.id}`
+    })
+  }
+  else {
+    WriteAdvice({
+      advice: 'Sorry, something went wrong! Try again later!',
+      number: 'ERROR!'
+    })
+  }
+  console.log(results, json);
+}
+
+function WriteAdvice(obj) {
+  const advice = document.querySelector('#advice');
+  const number = document.querySelector('#advice-id');
+
+  advice.innerHTML = obj.advice;
+  number.innerHTML = obj.number;
 }
 
 GetAdvice();
+
+document.querySelector('#button').addEventListener('click', GetAdvice);
